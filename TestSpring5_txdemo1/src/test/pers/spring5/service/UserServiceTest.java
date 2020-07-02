47290d4bf465d101f831be8caa0df42091b835e5
liuran <MsLiuRan@outlook.com>
2020-07-02 22:30:49 +0800

    spring5新特性

--- TestSpring5_txdemo1/src/test/pers/spring5/service/UserServiceTest.java
+++ TestSpring5_txdemo1/src/test/pers/spring5/service/UserServiceTest.java
@@ -2,7 +2,10 @@ package pers.spring5.service;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.transaction.PlatformTransactionManager;
+import pers.spring5.config.TxConfig;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +16,23 @@ public class UserServiceTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
         UserService userService = context.getBean("userService",UserService.class);
         userService.accountMoney();
+    }
+
+    @Test
+    public void accountMoney1() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean2.xml");
+        UserService userService = context.getBean("userService",UserService.class);
+        userService.accountMoney();
+    }
+
+    @Test
+    public void accountMoney2() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(TxConfig.class);
+        UserService userService = context.getBean("userService",UserService.class);
+        userService.accountMoney();
+    }
+
+    public static void main(String[] args) {
+
     }
 }
