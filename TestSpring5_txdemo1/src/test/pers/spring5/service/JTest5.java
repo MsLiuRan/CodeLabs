5cbb219a909e78aee2868c58468a60153b52ae8a
liuran <MsLiuRan@outlook.com>
2020-07-03 22:09:03 +0800

    junit5

--- TestSpring5_txdemo1/src/test/pers/spring5/service/JTest5.java
+++ TestSpring5_txdemo1/src/test/pers/spring5/service/JTest5.java
@@ -5,8 +5,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration("classpath:bean1.xml")
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration("classpath:bean1.xml")
+@SpringJUnitConfig(locations = "classpath:bean1.xml")
 public class JTest5 {
     @Autowired
     public UserService userService;
