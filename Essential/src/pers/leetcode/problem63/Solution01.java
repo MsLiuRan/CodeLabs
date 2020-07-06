7a7ed034920046e88adc94bb572997ec0037ba32
LiuRan <MsLiuRan@outlook.com>
2020-07-06 17:06:11 +0800

    动态规划

---
+++ Essential/src/pers/leetcode/problem63/Solution01.java
@@ -0,0 +1,33 @@
+package pers.leetcode.problem63;
+
+/**
+ * @ClassName Solution01 63.不同路径 II
+ * @Description 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/unique-paths-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author LiuRan
+ * @Date 2020/7/6 15:09
+ * @Version 1.0
+ */
+public class Solution01 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[] f = new int[m];
+        f[0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (obstacleGrid[i][j] == 1) {
+                    f[j] = 0;
+                    continue;
+                }
+                if (j - 1 >= 0 && obstacleGrid[i][j - 1] == 0) {
+                    f[j] += f[j - 1];
+                }
+            }
+        }
+        return f[m - 1];
+    }
+}
